2d72d4c0383875a6094a0c09fd7d1aeb45115042
staakaar <vancyroletaka@gmail.com>
2024-02-03 18:11:04 +0900

    add tcp segment

--- hoytcp/src/tcp.rs
+++ hoytcp/src/tcp.rs
@@ -19,4 +19,35 @@ const MAX_TRANSMITTION_TIMEOUT: u64 = 3;
 const MSS: usize = 1460;
 const PORT_RANGE: RANGE<u16> = 40000..60000;
 
+pub struct TCP {
+    sockets: HashMap<SockID, Socket>,
+}
 
+impl TCP {
+    pub fn new() -> Self {
+        let sockets = HashMap::new();
+        let tcp = Self { sockets };
+        tcp
+    }
+
+    fn select_unused_port(&self, rng: &mut ThreadRng) -> Result<u16> {
+        Ok(33445)
+    }
+
+    pub fn connect(&self, addr: Ipv4Addr, port: u16) -> Result<SockID> {
+        let mut rng = rand::tread_rng();
+        let mut socket = Socket::new(
+            get_source_addr_to(addr)?,
+            addr,
+            self.select_unused_port(&mut rng)?,
+        )?;
+        socket.send_tcp_packet(tcpflags::SYN, &[])?;
+        let sock_id = socket.get_sock_id();
+        Ok(sock_id)
+    }
+
+}
+
+fn get_source_addr_to(addr: Ipv4Addr) -> Result<Ipv4Addr> {
+    Ok("10.0.0.1".parse().unwrap())
+}
