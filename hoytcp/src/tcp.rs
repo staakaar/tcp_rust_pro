8668b7cccf12f8ad0e80d3a6569190c9dfedd80a
staakaar <vancyroletaka@gmail.com>
2024-02-05 00:26:15 +0900

    add receiver handler synsent handler

--- hoytcp/src/tcp.rs
+++ hoytcp/src/tcp.rs
@@ -1,7 +1,9 @@
 use crate::packet::TCPPacket;
-use crate::socket::{SockID, Socket, TcpStatus};
+use crate::socket::{self, SockID, Socket, TcpStatus};
 use crate::tcpflags;
-use anyhow::{Context, Result};
+use anyhow::{Context, Ok, Result};
+use pnet::packet::ip::IpNextHeaderProtocol;
+use pnet::packet::tcp::{TcpFlags, TcpPacket};
 use pnet::packet::{ip::IpNextHeaderProtocols, tcp::TCPPacket, Packet};
 use pnet::transport::{self, TransportChannelType};
 use rand::{rngs::ThreadRng, Rng};
@@ -20,18 +22,34 @@ const MSS: usize = 1460;
 const PORT_RANGE: RANGE<u16> = 40000..60000;
 
 pub struct TCP {
-    sockets: HashMap<SockID, Socket>,
+    sockets: RwLock<HashMap<SockID, Socket>>,
+    event_condvar: (Mutex<Option<TCPEvent>>, Condvar),
 }
 
 impl TCP {
-    pub fn new() -> Self {
-        let sockets = HashMap::new();
-        let tcp = Self { sockets };
+    pub fn new() -> Arc<Self> {
+        let sockets = RwLock::new(HashMap::new());
+        let tcp = Arc::new(Self {
+            sockets,
+            event_condvar: (Mutex::new(None), Condvar::new()),
+        });
+        let cloned_tcp = tcp.clone();
+        std::thread::spawn(move || {
+            cloned_tcp.receive_handler().unwrap();
+        });
         tcp
     }
 
     fn select_unused_port(&self, rng: &mut ThreadRng) -> Result<u16> {
-        Ok(33445)
+        for _ in 0..(PORT_RANGE.end - PORT_RANGE.start) {
+            let local_port = rng.get_range(PORT_RANGE);
+            let table = self.sockets.read().unwrap();
+
+            if table.keys().all(|k| local_port != k.2) {
+                return Ok(local_port);
+            }
+        }
+        anyhow::bail!("no available port found.");
     }
 
     pub fn connect(&self, addr: Ipv4Addr, port: u16) -> Result<SockID> {
@@ -40,14 +58,121 @@ impl TCP {
             get_source_addr_to(addr)?,
             addr,
             self.select_unused_port(&mut rng)?,
+            port,
+            TcpStatus::SynSent,
         )?;
-        socket.send_tcp_packet(tcpflags::SYN, &[])?;
+        socket.send_param.initial_seq = rng.gen_range(1..1 << 31);
+        socket.send_tcp_packet(socket.send_param.initial_seq, 0, tcpflags::SYN, &[])?;
+        socket.send_param.unacked_seq = socket.send_param.initial_seq;
+        socket.send_param.next = socket.send_param.initial_seq + 1;
+        let mut table = self.sockets.write().unwrap();
         let sock_id = socket.get_sock_id();
+        table.insert(sock_id, socket);
+        drop(table);
+
+        self.wait_event(sock_id, TCPEventKind::ConnectionCompleted);
         Ok(sock_id)
     }
 
+    fn receive_handler(&self) -> Result<()> {
+        dbg!("begin recv thread");
+        let (_, mut receiver) = 
+            transport::transport_channel(65535, TransportChannelType::Layer3(IpNextHeaderProtocols::Tcp)).unwrap();
+        
+        let mut packet_iter = transport::ipv4_packet_iter(&mut receiver);
+        loop {
+            let (packet, remote_addr) = match packet_iter.next() {
+                Ok((p, r)) => (p, r),
+                Err(_) => continue,
+            };
+
+            let local_addr = packet.get_destination();
+            let tcp_packet = match TcpPacket::new(packet.payload()) {
+                Some(p) => p,
+                None => { continue; }
+            };
+
+            let packet = TCPPacket::from(tcp_packet);
+            let remote_adddr = match remote_addr {
+                IpAddr::V4(addr) => addr, _ => { continue; }
+            };
+
+            let mut table = self.sockets.write().unwrap();
+            let socket = match table.get_mut(&SockID(local_addr, remote_adddr, packet.get_dest(), packet.get_src())) {
+                Some(socket) => socket,
+                None => match table.get_mut(&SockID(local_addr, UNDETERMINED_IP_ADDR, packet.get_dest(), UNDETERMINED_PORT)) {
+                    Some(socket) => socket,
+                    None => continue,
+                },
+            };
+
+            if !packet.is_correct_checksum(local_addr, remote_addr) {
+                dbg!("invalid checksum");
+                continue;
+            }
+
+            let sock_id = socket.get_sock_id();
+
+            if let Err(error) = match socket.status {
+                TcpStatus::SynSent => self.synsent_handler(socket, &packet), _ => {
+                    dbg!("not implemented state");
+                    Ok(())
+                }
+            } {
+                dbg!(error)
+            }
+        }
+    }
+
+    fn synsent_handler(&self, socket: &mut Socket, packet: &TCPPacket) -> Result<()> {
+        dbg!("synsent handler");
+
+        if packet.get_flag() & tcpflags::ACK > 0
+            && socket.send_param.unacked_seq <= packet.get_ack()
+            && packet.get_ack() <= socket.send_param.next
+            && packet.get_flag() & tcpflags::SYN > 0 {
+                socket.recv_param.next = packet.get_seq() + 1;
+                socket.recv_param.initial_seq = packet.get_seq();
+                socket.send_param.unacked_seq = packet.get_ack();
+                socket.send_param.window = packet.get_window_size();
+
+                if socket.send_param.unacked_seq > socket.send_param.initial_seq {
+                    socket.status = TcpStatus::Established;
+                    socket.send_tcp_packet(
+                        socket.send_param.next,
+                        socket.recv_param.next,
+                        tcpflags::ACK,
+                        &[],
+                    )?;
+                    dbg!("status: synsent ->", &socket.status);
+                    self.publish_event(socket.get_sock_id(), TCPEventKind::ConnectionCompleted);
+                } else {
+                    socket.status = TcpStatus::SynRcvd;
+                    socket.send_tcp_packet(
+                        socket.send_param.next,
+                        socket.recv_param.next,
+                        tcpflags::ACK,
+                        &[],
+                    )?;
+                }
+            dbg!("status synsent ->", &socket.status);
+        }
+        Ok(())
+    }
 }
 
+
 fn get_source_addr_to(addr: Ipv4Addr) -> Result<Ipv4Addr> {
-    Ok("10.0.0.1".parse().unwrap())
+    let output = Command::new("sh").arg("-c").arg(format!("ip route get {} | grep src", addr)).output()?;
+    let mut output = str::from_utf8(&output.stdout)?.trim().split_ascii_whitespace();
+
+    while let Some(s) = output.next() {
+        if s == "src" {
+            break;
+        }
+    }
+
+    let ip = output.next().context("failed to get src ip")?;
+    dbg!("source addr", ip);
+    ip.parse().context("failed to parse source ip")
 }
